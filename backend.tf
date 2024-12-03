af3c6f3727da6df9cb706a97bc9783cac815513c
Felipe Pinheiro Dantas <felipehyo@gmail.com>
2024-12-03 09:21:53 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-s3-bucket-pos-tech-postgres"
+    bucket         = "rds-fiap-bucket"
     key            = "terraform/state/rds.tfstate"
     region         = "sa-east-1"
     encrypt        = true
